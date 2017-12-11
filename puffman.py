329d1318c7d42aa1d86b7069b74a5578952235a5
BrennaManning <brenna.manning@students.olin.edu>
2017-12-11 01:18:53 -0500

    added to_array

--- puffman.py
+++ puffman.py
@@ -19,10 +19,40 @@ def puffman(img, decode_dict):
 		if curr in decode_dict:
 			res.append(decode_dict[curr])
 			curr = ''
-	print res
 	return res
 
+def to_array(img, dimensions):
+	""" Converts 1d img array into 2d array according to dimensions """
+
+	# NEEDS TEST
+
+	if len(dimensions) != 16:
+		print('Error: dimensions should be 16 bits long,', 
+			'containing an 8 bit number for h and an 8 bit number for w')
+	# Retreive h and w dimensions from bits in array 
+	h = ''
+	w = ''
+	for i in range(8):
+		h += str(dimensions[i])
+		w += str(dimensions[i+8])
+
+	h = int(h,2)
+	w = int(w,2)
+
+	res = np.zeros((h,w))
+
+	for row_index in range(0, h):
+		for col_index in range(0, w):
+			res[row_index][col_index] = img[col_index + row_index * w]
+
+	return res
+			
 if __name__ == '__main__':
 
-	puffman({'010':5, '011': 7, '1':2}, np.array([1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]))
-	# expect to return [2 5 7 5 2] 
+	p = puffman(np.array([1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1]), {'010':5, '011': 7, '1':2},)
+	print p
+	# expect puffman to return [2 5 7 5 2 2] 
+	a = to_array(p, np.array([0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0]))
+	print a
+	# expect to return [[2 5] [7 5] [2 2] 
+	
