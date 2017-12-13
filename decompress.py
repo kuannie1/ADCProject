caff28b4b0b8de2dd1663b7c94819f47092f3ae2
BrennaManning <brenna.manning@students.olin.edu>
2017-12-12 23:29:42 -0500

    Add files via upload

--- decompress.py
+++ decompress.py
@@ -20,7 +20,7 @@ from scipy import stats
 import data_processing
 import puffman
 import image_processing
-from find_start_end_signal import *
+#from find_start_end_signal import *
 
 
 
@@ -29,19 +29,24 @@ def main(filename, header, start_index = 0, end_index = 0):
 
     data = read_from_file(filename)
     data = data_processing.decomplexize_data(data)
-    data = np.real(data)
+    print('decomplexized')
     if end_index != 0:
-	    data = data[start_index:end_index]
+        data = data[start_index:end_index]
+    data = data_processing.estimate_transmitted_signal(data)
+    print('estimated transmitted signal')
     data = data_processing.unexpand_and_correct(data)
+    print('unexpanded')
     [dimensions, encoded_img, decode_dict] = data_processing.data_from_array(data, header)
+    print('data to array')
     np.set_printoptions(threshold='nan')
     decode_dict = data_processing.binary_to_dictionary(decode_dict)
+    print('decode_dict')
     img_1d = puffman.puffman(encoded_img, decode_dict)
     print('len_1d_img =', len(img_1d))
     print('dimensions =', dimensions)
     img = puffman.to_array(img_1d, dimensions)
 
-    image_processing.save_image(img)
+    image_processing.save_image(img, 'super_decompressed_doge.jpg')
 
     print 'done'
     return [decode_dict, img]
@@ -52,8 +57,6 @@ def read_from_file(filename):
     fileobj2.close()
     return off
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 def compare_rx_tx(received, original):
     size = received.size
@@ -65,41 +68,8 @@ def compare_rx_tx(received, original):
     xor_rx_tx = np.logical_xor(received[0:size], original[0:size])
     print np.invert( xor_rx_tx )
 
-=======
-=======
->>>>>>> 32ab80b5a1049519fda7702d891c1785131daf34
-
-def compare_rx_tx(received, original):
-    size = received.size
-    if (received.size != original.size):
-        print "sizes are different"
-        if (received.size>original.size):
-            size = original.size 
-    # use logical xor to compare received & original vectors of length size
-    xor_rx_tx = np.logical_xor(received[0:size], original[0:size])
-    print np.invert( xor_rx_tx )
-
-
-if __name__ == '__main__':
-
-    y = read_from_file('superlong50.dat')
-    (start_i, end_i) = find_start_end_signal('superlong50.dat')
-    y = data_processing.decomplexize_data(y)
-    y = y[start_i:end_i]
-        #y = y[1634000+332 - 1:1634000+3314 - 1]
-        #y = y[252800 + 30-1 : 252800 + 612-1]
-    y = data_processing.estimate_transmitted_signal(y)
-    plt.plot(y)
-    plt.show()
-    # y = data_processing.unexpand_and_correct(y)
-    # print "length of y: ", len(y)
-    # np.set_printoptions(threshold='nan')
-    # original_signal = np.array([0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1])
-    # print "length of original signal: ", len(original_signal)
-    # print compare_rx_tx(y, original_signal) # first checking if input & output have same size. Return a np vector with 1 -> same and 0 -> not same
-    # print y
 
 if __name__ == '__main__':
 	header = np.zeros(200)
 	header[41] = 1
-	main('notransmission.dat', header)
+	main('receivedDoge.dat', header, start_index=1156245, end_index=6949292)
