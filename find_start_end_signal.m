5a0e1b7fd597be9d14af54e0cbf8de66b9f7af1a
kuannie1 <anne.ku4@gmail.com>
2017-12-12 21:49:33 -0500

    modified start index to avoid blip at beginning

--- find_start_end_signal.m
+++ find_start_end_signal.m
@@ -1,26 +1,28 @@
 function y = find_start_end_signal()
-    f1 = fopen('superlong50.dat', 'r');
+    f1 = fopen('sixtythousandbits.dat', 'r');
     tmp = fread(f1,'float32');
     fclose(f1);
     y = tmp(1:2:end)+1i*tmp(2:2:end);
     
     real_y = real(y); % getting the real part of the y vector
+    plot(real_y);
+    
+    length(real_y)
+    
     start_of_signal = 0; % going to be the first index
-    for i=1:length(real_y)
-      if (abs(real_y(i)) > 0.0025)
-        start_of_signal = i-3;
+    for i=2000:length(real_y)
+      if (abs(real_y(i)) > 0.002)
+        start_of_signal = i-5
         break
       end
     end
     
     end_of_signal = length(real_y);
-    for i = length(real_y):-1:1
-      if (abs(real_y(i)) > 0.0025)
-        end_of_signal = i+3;
+    for i = length(real_y):-1:5
+      if (abs(real_y(i)) > 0.002)
+        end_of_signal = i+5
         break
       end
     end
     plot(real_y(start_of_signal:end_of_signal));
-    figure 
-    plot(real_y(start_of_signal-10000:end_of_signal+10000));
 end
